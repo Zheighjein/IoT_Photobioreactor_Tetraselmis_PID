f9d6feb25a1865965f7f7c5cb2d4447153dce77c
Zheighjein <zheighjeinlhorraine@gmail.com>
2026-04-26 04:08:18 +0800

    tuning switching fixed 2

--- hardware_main.py
+++ hardware_main.py
@@ -48,7 +48,7 @@ if USE_AUTOTUNE:
 
     insert_event(1, "SYSTEM", "Starting autotune", "Relay tuning", "adjusting")
 
-    AUTOTUNE_DURATION = 20  # testing
+    AUTOTUNE_DURATION = 20  # testing #make this into 86400 for a full day
     autotune_start = time.time()
 
     while time.time() - autotune_start < AUTOTUNE_DURATION:
